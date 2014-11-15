291595b16420f6ac4a206e512524f54799bc7e2c
Simon Altschuler <simon@altschuler.dk>
2014-11-15 21:00:18 +0100

    add product category hover menu

--- verellen/content/context_preprocessor.py
+++ verellen/content/context_preprocessor.py
@@ -1,9 +1,12 @@
 import models
 
 from utils import get_content_model
+from products.models import Category
 
 def content(request):
+    product_categories = Category.objects.all()
     return {
         'footer_content': get_content_model(models.FooterContent),
         'menu_content': get_content_model(models.MenuContent),
+        'product_categories': product_categories,
     }

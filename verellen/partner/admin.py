46ca2cfc9aa41a18691618350f558577d0439110
Simon Altschuler <simon@altschuler.dk>
2014-11-24 15:20:06 +0100

    add partner information to user model

--- verellen/partner/admin.py
+++ verellen/partner/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from partner.models import Partner, PartnerType
+
+class UserInline(admin.TabularInline):
+    model = User
+
+class PartnerTypeAdmin(admin.ModelAdmin):
+    fields = [ 'name' ]
+
+class PartnerInline(admin.TabularInline):
+    model = Partner
+    can_delete = False
+
+class UserAdmin(UserAdmin):
+    inlines = (PartnerInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+admin.site.register(PartnerType, PartnerTypeAdmin)
--- verellen/partner/models.py
+++ verellen/partner/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PartnerType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+class Partner(models.Model):
+    user = models.OneToOneField(User)
+    partner_type = models.ForeignKey(PartnerType, blank=False, null=False)
+
+    def __unicode__(self):
+        return "{0} ({1})".format(self.user.username, self.partner_type)

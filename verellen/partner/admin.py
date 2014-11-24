f5e57ef7325b2ad80015919b2aaa26c889a73c90
Simon Altschuler <simon@altschuler.dk>
2014-11-24 15:52:08 +0100

    rename PartnerType to PartnerGroup

--- verellen/partner/admin.py
+++ verellen/partner/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from partner.models import Partner, PartnerType
+from partner.models import Partner, PartnerGroup
 
 class UserInline(admin.TabularInline):
     model = User
 
-class PartnerTypeAdmin(admin.ModelAdmin):
+class PartnerGroupAdmin(admin.ModelAdmin):
     fields = [ 'name' ]
 
 class PartnerInline(admin.TabularInline):
@@ -15,9 +15,18 @@ class PartnerInline(admin.TabularInline):
     can_delete = False
 
 class UserAdmin(UserAdmin):
-    inlines = (PartnerInline, )
+    inlines = (PartnerInline,)
+    list_display = UserAdmin.list_display + ('list_display_partner',)
+
+    def list_display_partner(self, user):
+        if user.partner:
+            return user.partner.group.name
+        else:
+            return None
+
+    list_display_partner.short_description = "Partner group"
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
-admin.site.register(PartnerType, PartnerTypeAdmin)
+admin.site.register(PartnerGroup, PartnerGroupAdmin)
--- verellen/partner/models.py
+++ verellen/partner/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class PartnerType(models.Model):
+
+class PartnerGroup(models.Model):
     name = models.CharField(max_length=255)
 
     def __unicode__(self):
         return self.name
 
+class Download(models.Model):
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to='downloads')
+    partner_group = models.ForeignKey(PartnerGroup, blank=False, null=False)
+
 class Partner(models.Model):
     user = models.OneToOneField(User)
-    partner_type = models.ForeignKey(PartnerType, blank=False, null=False)
+    group = models.ForeignKey(PartnerGroup, blank=False, null=False)
 
     def __unicode__(self):
-        return "{0} ({1})".format(self.user.username, self.partner_type)
+        return "{0} ({1})".format(self.user.username, self.group)

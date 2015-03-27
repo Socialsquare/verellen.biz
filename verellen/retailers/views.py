e364eb87e0fe702376720b35a0c7f42c71e376c4
madslundt <madssx@hotmail.com>
2015-03-27 12:50:05 +0100

    Appended regex to retailer locator

--- verellen/retailers/views.py
+++ verellen/retailers/views.py
@@ -12,12 +12,21 @@ def home(request):
     if 'query' in request.GET.keys():
         showing_all = False
         query = request.GET['query']
+        split = query.split(',')
+        regex = '^.*(%s).*$' % '|'.join(split)
         matches = Retailer.objects.filter(Q(partner__name__contains=query)
                                           | Q(address__contains=query)
                                           | Q(city__contains=query)
                                           | Q(phone__contains=query)
                                           | Q(state__contains=query)
-                                          | Q(zip_code__contains=query))
+                                          | Q(zip_code__contains=query)
+                                          | Q(partner__name__iregex=regex)
+                                          | Q(address__iregex=regex)
+                                          | Q(city__iregex=regex)
+                                          | Q(phone__iregex=regex)
+                                          | Q(state__iregex=regex)
+                                          | Q(zip_code__iregex=regex)
+                                          )
 
     return render(request, 'retailers/home.html', {
         'matches': matches,

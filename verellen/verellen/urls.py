9402e1f61d375b733420f085ae102467f118467a
Andreas Larsen <andreas@larsenwork.com>
2016-12-21 16:50:54 +0100

    Updated urls

--- verellen/verellen/urls.py
+++ verellen/verellen/urls.py
@@ -20,16 +20,19 @@ urlpatterns = [
 
     url(r'(pdfjs/web/viewer\.html.*)$', TemplateView.as_view(template_name='pdfjs/web/viewer.html')),
 
+    # Used to be in partner section but re-reouted because now publicly visible
+    url(r'^resources$', partner_views.sales_tools, name='partner.sales_tools'),
+    url(r'^products$', partner_views.product_category_list, name='partner.product_category_list'),
+    url(r'^products/(?P<category_slug>.*)$', partner_views.product_category, name='partner.product_category'),
+    url(r'^product/(?P<product_id>.*)$', partner_views.product_detail, name='partner.product_detail'),
+    url(r'^search/$', partner_views.search, name='partner.search'),
+    url(r'^view$', partner_views.view_spreadsheet, name='partner.view_spreadsheet'),
+
     url(r'^partner/$', partner_views.home, name='partner.home'),
     url(r'^partner/home/$', partner_views.home, name='partner.home'),
-    url(r'^partner/search/$', partner_views.search, name='partner.search'),
     url(r'^partner/login/$', partner_views.do_login, name='partner.login'),
     url(r'^partner/logout/$', partner_views.do_logout, name='partner.logout'),
     url(r'^partner/login_form$', partner_views.login_form, name='partner.login_form'),
-
-    url(r'^partner/products$', partner_views.product_category_list, name='partner.product_category_list'),
-    url(r'^partner/products/(?P<category_slug>.*)$', partner_views.product_category, name='partner.product_category'),
-    url(r'^partner/product/(?P<product_id>.*)$', partner_views.product_detail, name='partner.product_detail'),
 
     url(r'^partner/download/us$', partner_views.download_products_us, name='partner.download_products_us'),
     url(r'^partner/download/eu$', partner_views.download_products_eu, name='partner.download_products_eu'),
@@ -38,14 +41,12 @@ urlpatterns = [
     url(r'^partner/download/eu/(?P<category_slug>.*)$', partner_views.download_categories_eu, name='partner.download_categories_eu'),
 
     url(r'^partner/price_lists$', partner_views.price_lists, name='partner.price_lists'),
-    url(r'^partner/sales_tools$', partner_views.sales_tools, name='partner.sales_tools'),
     url(r'^partner/account/$', partner_views.account, name='partner.account'),
     url(r'^partner/account_update/$', partner_views.account_update, name='partner.account_update'),
-    url(r'^partner/view$', partner_views.view_spreadsheet, name='partner.view_spreadsheet'),
 
-    url(r'^products/$', product_views.home, name='product.home'),
-    url(r'^products/(?P<category_slug>.*)$', product_views.list, name='product.list'),
-    url(r'^product/(?P<product_id>\d+)$', product_views.detail, name='product.detail'),
+    # url(r'^products/$', product_views.home, name='product.home'),
+    # url(r'^products/(?P<category_slug>.*)$', product_views.list, name='product.list'),
+    # url(r'^product/(?P<product_id>\d+)$', product_views.detail, name='product.detail'),
 
     url(r'^about/$', content_views.about, name='content.about'),
 
